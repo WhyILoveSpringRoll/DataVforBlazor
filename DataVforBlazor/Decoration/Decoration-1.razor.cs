da75ed94af1ece306805ec4e8f124812f71cabdf
Liyiming <eebbk@163.com>
2022-08-25 17:16:15 +0800

    js不是人写的

---
+++ DataVforBlazor/Decoration/Decoration-1.razor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVforBlazor
+{
+    public partial class Decoration_1:DecorationBase
+    {
+        public override List<string> MergedColor { get; set; } = new List<string> { "#fff", "#0de7c2" };
+
+        private int rowNum = 4;
+        private int rowPoints = 20;
+        private List<int> svgWH = new List<int> { 200, 50 };
+        private List<int> svgScale = new List<int> { 1, 1 };
+        private double pointSideLength = 2.5;
+        private double halfPointSideLength = 1.25;
+        private List<Point> points = new List<Point>();
+        private Point[] rects = new Point[] { };
+        private Random rd = new Random(Guid.NewGuid().GetHashCode());
+
+
+        protected override Task OnInitializedAsync()
+        {
+            CalcSVGData();
+            return base.OnInitializedAsync();
+        }
+        private void CalcSVGData()
+        {
+            CalcPointsPosition();
+            CalcRectsPosition();
+        }
+        private void CalcPointsPosition()
+        {
+            points.Clear();
+            var horizontalGap = svgWH[0] / (rowPoints + 1);
+            var verticalGap = svgWH[1] / (rowNum + 1);
+            for(int i=1;i<=rowNum;i++)
+            {
+                for(int j=1;j<=rowPoints;j++)
+                {
+                    points.Add(new Point(horizontalGap * i, verticalGap * j));
+                }
+            }
+        }
+        private void CalcRectsPosition()
+        {
+            Point p1 = points[rowPoints * 2 - 1];
+            Point p2 = points[rowPoints * 2 - 3];
+            rects = new Point[] { p1, p2 };
+        }
+    }
+}
---
+++ DataVforBlazor/Decoration/DecorationBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVforBlazor
+{
+    public class DecorationBase:ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public int Height { get; set; } = 0;
+        protected string height;
+        [Parameter]
+        public int Width { get; set; } = 0;
+        protected string width;
+        [Parameter]
+        public virtual double Dur { get; set; } = 3;
+        [Parameter]
+        public virtual List<string> MergedColor { get; set; } = new List<string> { "#4fd2dd", "#235fa7" };
+        protected string ID = Guid.NewGuid().ToString();
+    }
+}
