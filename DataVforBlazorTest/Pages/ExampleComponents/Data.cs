760c3d5cc132fac902444573e72a74810ee88f90
Liyiming <eebbk@163.com>
2022-09-07 14:45:46 +0800

    Add flylinechart docment

---
+++ DataVforBlazorTest/Pages/ExampleComponents/Data.cs
@@ -0,0 +1,18 @@
+﻿namespace DataVforBlazorTest
+{
+    using System.ComponentModel;
+    public class Data
+    {
+        [DisplayName("参数")]
+        public string Param { get; set; }
+
+        [DisplayName("说明")]
+        public string Info { get; set; }
+
+        [DisplayName("类型")]
+        public string Type { get; set; }
+
+        [DisplayName("默认值")]
+        public string DefaultValue { get; set; }
+    }
+}
