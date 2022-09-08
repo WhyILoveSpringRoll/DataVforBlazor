9ffb18e4662403021ccf3a01f352a9c4c6dd5263
Liyiming <eebbk@163.com>
2022-09-08 16:17:45 +0800

    Add ConicalColumnChart

---
+++ DataVforBlazor/ConicalColumnChart/ConicalColumnChartConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVforBlazor
+{
+    public class ConicalColumnChartConfig
+    {
+        public int fontsize { get; set; } = 12;
+        public int imgSideLength { get; set; } = 30;
+
+        public string columnColor { get; set; } = "rgba(0, 194, 255, 0.4)";
+
+        public string textColor { get; set; } = "#fff";
+        public bool showValue { get; set; } = false;
+
+        public List<string> img { get; set; } = new List<string>();
+
+        public Dictionary<string, double> data =new Dictionary<string, double>();
+    }
+}
