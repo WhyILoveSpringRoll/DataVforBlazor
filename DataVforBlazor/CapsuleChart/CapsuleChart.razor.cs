fec7707dd89ba07e6a7b6c24a6a71706b2125639
Liyiming <eebbk@163.com>
2022-09-13 17:23:12 +0800

    Add CaspsuleChart

---
+++ DataVforBlazor/CapsuleChart/CapsuleChart.razor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVforBlazor
+{
+    public partial class CapsuleChart
+    {
+        [Parameter]
+        public CapsuleChartConfig Config { get; set; }
+        [Parameter]
+        public int Height { get; set; } = 0;
+        protected string height;
+        [Parameter]
+        public int Width { get; set; } = 0;
+        protected string width;
+        private readonly string ID = Guid.NewGuid().ToString("N");
+        private List<double> capsuleValue = new List<double>();
+        private List<double> capsuleLength = new List<double>();
+        private List<string> labelData = new List<string>();
+        private List<double> labelDataLength = new List<double>();
+
+        [Inject]
+        protected IJSRuntime jsRuntime { get; set; }
+        protected override async Task OnParametersSetAsync()
+        {
+            Lazy<Task<IJSObjectReference>> moduleTask;
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            "import", "./_content/DataVforBlazor/DataVforBlazorInterop.js").AsTask());
+            var module = await moduleTask.Value;
+            if (Width <= 0)
+            {
+                Width = int.Parse((await module.InvokeAsync<object>("GetWidth", ID)).ToString());
+            }
+            if (Height <= 0)
+            {
+                Height = int.Parse((await module.InvokeAsync<object>("GetHeight", ID)).ToString());
+            }
+            height = Height <= 0 ? "" : Height + "px";
+            width = Width <= 0 ? "" : Width + "px";
+
+            CalcCapsuleLengthAndLabelData();
+        }
+
+        private void CalcCapsuleLengthAndLabelData()
+        {
+            if(Config.data.Count==0)
+                return;
+
+            this.capsuleValue = Config.data.Select(i => i.value).ToList();
+            var maxValue = Config.data.Max(i => i.value);
+            this.capsuleLength = capsuleValue.Select(i => i / 1.0 / maxValue).ToList();
+            var oneFifth = maxValue / 5;
+            this.labelData=Enumerable.Range(0, 6).Select(i => Math.Ceiling(i * oneFifth)).ToList();
+            this.labelDataLength = Enumerable.Range(0, labelData.Count()).Select(i=>i/1.0/maxValue).ToList();
+        }
+
+
+    }
+}
---
+++ DataVforBlazor/CapsuleChart/CapsuleChartConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVforBlazor
+{
+    public class CapsuleChartConfig
+    {
+        public string unit { get; set; }
+        public bool showValue { get; set; } = false;
+
+        public List<string> colors { get; set; } = new List<string> { "#37a2da", "#32c5e9", "#67e0e3", "#9fe6b8", "#ffdb5c", "#ff9f7f", "#fb7293" };
+
+        public List<CapsuleData> data = new List<CapsuleData>();
+    }
+
+    public class CapsuleData
+    {
+        public string name { get; set; }
+        public double value { get; set; }
+        internal double length { get; set; }
+    }
+}
