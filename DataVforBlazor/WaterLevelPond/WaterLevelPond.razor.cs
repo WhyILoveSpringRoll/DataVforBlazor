41c879dd7f74f24a3518b20363e9febdf1dabbb4
Liyiming <eebbk@163.com>
2022-09-14 14:29:52 +0800

    Add WaterLevelPond

---
+++ DataVforBlazor/WaterLevelPond/WaterLevelPond.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVforBlazor
+{
+    public partial class WaterLevelPond
+    {
+        public int Height { get; set; } = 0;
+        protected string height;
+        [Parameter]
+        public int Width { get; set; } = 0;
+        protected string width;
+        
+        [Parameter]
+        public WaterLevelPondConfig Config { get; set; } = new WaterLevelPondConfig();
+        protected string ID = Guid.NewGuid().ToString();
+        private string gradientId = "";
+        [Inject]
+        protected IJSRuntime jsRuntime { get; set; }
+        protected override async Task OnParametersSetAsync()
+        {
+            Lazy<Task<IJSObjectReference>> moduleTask;
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            "import", "./_content/DataVforBlazor/DataVforBlazorInterop.js").AsTask());
+            var module = await moduleTask.Value;
+            if (Width <= 0)
+            {
+                Width = int.Parse((await module.InvokeAsync<object>("GetWidth", ID)).ToString());
+            }
+            if (Height <= 0)
+            {
+                Height = int.Parse((await module.InvokeAsync<object>("GetHeight", ID)).ToString());
+            }
+            height = Height <= 0 ? "" : Height + "px";
+            width = Width <= 0 ? "" : Width + "px";
+            
+            gradientId = $"water-level-pond-{ID}";
+        }
+
+    }
+}
---
+++ DataVforBlazor/WaterLevelPond/WaterLevelPondConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataVforBlazor
+{
+    public class WaterLevelPondConfig
+    {
+        public List<double> data = new List<double>();
+        public int shape { get; set; } = WaterLevelPondShape.Rect;
+
+        public List<string> colors { get; set; } = new List<string> { "#00BAFF", "#3DE7C9" };
+
+        public int waveNum { get; set; } = 3;
+        public int waveHeight { get; set; } = 40;
+        public double waveOpacity { get; set; } = 0.4;
+        public string formatter = "";
+    }
+    public readonly struct WaterLevelPondShape
+    {
+        public static int Rect = 0;
+        public static int RoundRect = 1;
+        public static int Round = 2;
+    }
+}
